496ba13695d26958c05ab8bc984f350208d6bfb2
Lucien <lucien.lebas@ecole2600.com>
2024-03-08 11:11:57 +0100

    petites modif

--- main.c
+++ main.c
@@ -3,15 +3,25 @@
 #include <string.h>
 uint64_t challenge(uint64_t input);
 
-int main(){
-    uint8_t car = 77;
-    car = fgetc(stdin);
-    if(challenge(car)==0xf3d66f147a312b13){
-        printf("Congrats");
+uint64_t validtable[32] = {0xc9c30458ae310b48, 0x3a156b4cd400205b};
+int main(int argc, char **argv){
+    uint8_t valid = 0;
+    char buffer[32] = {0};
+    // fgets(buffer, 32, stdin);
+    
+    for (uint8_t i = 0; i < 0xff; i++)
+    {
+        printf("%X: %lX\n", i, challenge(i));
+        
     }
-    else{
-        printf("Nope...");
-    }
+    printf("%X: %lX\n", 0xff, challenge(0xff));
+                      
+    // if(challenge('M')==0xf3d66f147a312b13){
+    //     printf("Congrats");
+    // }
+    // else{
+    //     printf("Nope...");
+    // }
     return 1;
     // char key[] = "MIDNIGHT{COUCOULESBOSS}";
     // uint64_t t = 0;
@@ -26,5 +36,5 @@ int main(){
     //     printf("%c: %lX\n", key[i], challenge(key[i]));
     //     t = 0;
     // }
-    return 1;
+    // return 1;
 }
