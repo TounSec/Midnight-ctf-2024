0f6addf5c7ec6ca4cac67f39d25aa62955dd15b6
pwnsec <pwnsec@reverse-sec.lan>
2024-03-09 12:07:16 -0500

    Midnight CTF : Packer

--- rust-packer/src/main.rs
+++ rust-packer/src/main.rs
@@ -1,30 +1,52 @@
 use std::{
-    env,
     fs::File,
-    io::{self, Read, Write},
-    path::Path
+    io::Read,
+    env::args,
+    process::exit
 };
+use libc::{mmap, munmap, MAP_ANONYMOUS, MAP_FAILED, MAP_PRIVATE, PROT_EXEC, PROT_READ};
 
-fn main() -> io::Result<()>
+fn main()
 {
-    let args = env::args().collect::<Vec<String>>();
-
-    if args.len() != 3 {
-        eprintln!("Usage : PACKER <input_binary> <output_binary>");
-        return Ok(());
+    let args = args().collect::<Vec<String>>();
+    
+    if args.len() != 2 {
+        eprintln!("Usage : rust-packer <input_binary>");
+        exit(1);
     }
 
-    let input_path = Path::new(&args[1]);
-    let output_path = Path::new(&args[2]);
-    let mut input_binary = File::open(&input_path)?;
-    let mut input_data = Vec::new();
-    
-    input_binary.read_to_end(&mut input_data)?;
-    
-    let mut output_binary = File::create(&output_path)?;
-    output_binary.write_all(b"PACKED\n")?;
-    output_binary.write_all(&input_data)?;
+    let buffer = load_binary_memory(args[1].clone()).ok().unwrap();
+    //println!("{:?}", buffer);
 
-    println!("Packing completed successfully");
-    Ok(())
+    let addr = unsafe {
+        mmap(std::ptr::null_mut(),
+        buffer.len(),
+        PROT_READ | PROT_EXEC,
+        MAP_PRIVATE | MAP_ANONYMOUS,
+        -1,
+        0)
+    };
+    println!("{:?}", addr);
+
+    if addr == MAP_FAILED {
+        eprintln!("Failed to map memory");
+        exit(1);
+    }
+
+    unsafe {
+        std::ptr::copy_nonoverlapping(buffer.as_ptr(), addr as *mut u8, buffer.len());
+
+        let func: extern "C" fn() = std::mem::transmute(addr);
+        func();
+
+        munmap(addr, buffer.len());
+    }
+    
+}
+
+fn load_binary_memory(path: String) -> std::io::Result<Vec<u8>>{
+    let mut file = File::open(&path)?;
+    let mut buffer = Vec::new();
+    file.read_to_end(&mut buffer)?;
+    Ok(buffer)
 }
